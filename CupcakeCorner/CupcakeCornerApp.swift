94c10cf10e0b53e2822d6d9385ad0b3e624fa727
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-09 17:50:36 -0800

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jasper Tan on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
