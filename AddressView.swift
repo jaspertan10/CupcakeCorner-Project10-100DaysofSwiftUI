4273e0270301b4393c5f4ac2ae349adcf08a0a65
Jasper Tan <jatan@Jaspers-Mac-mini.local>
2024-12-10 23:36:04 -0800

    Cupcake order UI and logic, add AddressView and CheckoutVIew

---
+++ AddressView.swift
@@ -0,0 +1,39 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Jasper Tan on 12/10/24.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    
+    @Bindable var order: Order
+    
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ Order.swift
@@ -0,0 +1,84 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Jasper Tan on 12/10/24.
+//
+
+import SwiftUI
+
+@Observable
+class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
+    /* Cupcake types & selection*/
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    var type = 0
+    
+    /* Quantity */
+    var quantity = 3
+    
+    
+    /* Special requests */
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    
+    // Address properties
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    
+    // Determines if address is valid
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    // Determines cost of order
+    var cost: Decimal {
+        
+        // 2 per cupcake
+        var cost = Decimal(quantity) * 2
+        
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+        
+        // $1 per cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        // .50 per cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
+    
+}
